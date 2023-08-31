437c07f41e4ac9bca831d774ba08ceb39afaf6c9
Tiago Rios da Rocha <tiagoriosrocha@gmail.com>
2023-08-31 10:17:17 -0300

    link menu

--- resources/views/estande/show.blade.php
+++ resources/views/estande/show.blade.php
@@ -26,11 +26,11 @@
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 link-secondary">Dashboard</a></li>
+          <li><a href="/dashboard" class="nav-link px-2 link-secondary">Dashboard</a></li>
         </ul>
 
         <div class="dropdown text-end">
-          <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <a href="/dashboard" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
             <img src="/images/{{ Auth::user()->imagem }}" alt="mdo" width="32" height="32" class="rounded-circle">
           </a>
           <ul class="dropdown-menu text-small">
